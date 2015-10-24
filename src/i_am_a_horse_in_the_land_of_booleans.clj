756cf12ec61ca2d306476dc7fb54a485d1763d4e
gpwclark <gpwclark@gmail.com>
2015-10-23 21:10:53 -0400

    Wrote a few more functions today

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,33 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (> x 0)
+    x
+    (* -1 x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (> (mod n divisor) 0)
+    false
+    true
+    ))
 
 (defn fizzbuzz [n]
-  ":(")
+  (let  [three (divides? 3 n) five (divides? 5 n)]
+  (cond
+   (and three five) "gotcha!"
+   three "fizz"
+   five "buzz"
+   :else "")))
 
 (defn teen? [age]
-  ":(")
+  (if (and (> age 12) (< age 20))
+    true
+    false)
+  )
+
 
 (defn not-teen? [age]
   ":(")
